e34df0e3fcc35d5ea85be42bc5da317f3bbd85c2
Bobach22 <l.user2200@gmail.com>
2021-07-16 02:10:09 +0500

    bind controller methods

--- src/controllers/image.controller.ts
+++ src/controllers/image.controller.ts
@@ -1,17 +1,28 @@
 import axios from 'axios';
+import * as typeorm from 'typeorm';
 import {Request, Response, NextFunction} from 'express';
 import { Image } from '../models/image.entity';
 import {DogResponse} from '../common/interfaces/resource'
 import CONFIG from '../config';
 import { ImageRepository } from '../models/image.repository';
 import { getRepository } from 'typeorm';
+import { ImageDataRead } from '../models/image.data';
 
 export default class ImageController{
 
-    public async index(){
-        const images:Image[]=[];
+    public imageRepository: ImageRepository;
 
-        return images;
+    public constructor(imageRepository: ImageRepository){
+        this.imageRepository=imageRepository;
+        this.index=this.index.bind(this)
+        this.store=this.store.bind(this)
+    }
+
+    public async index(req: Request, res: Response){
+        let images=await this.imageRepository.findAll();
+        
+
+        return res.send(images.map(image=>image.buildData()));
     }
 
     public async store(req: Request, res: Response){
@@ -24,8 +35,9 @@ export default class ImageController{
         newImage.created_at=new Date();
         
         const imageRepository=getRepository(Image);
+        const data=await imageRepository.save(newImage)
 
-        return imageRepository.save(newImage);
+        return res.send(data);
     }
 
 }
